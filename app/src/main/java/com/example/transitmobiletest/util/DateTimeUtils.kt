726a2b3e39472e44a8c8bd5b8063783d128fa2f8
Arkillon <Ma_paquin@hotmail.com>
2023-11-10 10:21:40 -0500

    fix possible crash with date parsing

--- app/src/main/java/com/example/transitmobiletest/util/DateTimeUtils.kt
+++ app/src/main/java/com/example/transitmobiletest/util/DateTimeUtils.kt
@@ -4,8 +4,13 @@ import java.text.SimpleDateFormat
 
 object DateTimeUtils {
     fun getFormattedDateTime(dateTime: String) : String{
-        val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-        val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm")
-        return formatter.format(parser.parse(dateTime))
+        try {
+            val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm")
+            return formatter.format(parser.parse(dateTime))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return dateTime
+        }
     }
 }
